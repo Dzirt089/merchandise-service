ab6a962530c829d1814bfc3cb745a8bd1fd42750
Илья Емец <suoza089@gmail.com>
2024-04-29 22:20:43 +0300

    Добавил Middleware компоненты. На логгирование  request и response, версионирование.

---
+++ OzonEdu.Infrastructure/Configuration/Middlewares/ResponseLoggingMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OzonEdu.Infrastructure.Configuration.Middlewares
+{
+    public class ResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ResponseLoggingMiddleware> _logger;
+        public ResponseLoggingMiddleware(RequestDelegate next,
+            ILogger<ResponseLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await LogResponse(context);
+            await _next(context);
+        }
+
+        private async Task LogResponse(HttpContext context)
+        {
+            try
+            {
+                if (context.Response.ContentLength > 0)
+                {
+                    var buffer = new byte[context.Response.ContentLength.Value];
+                    await context.Response.Body.ReadAsync(buffer, 0, buffer.Length);
+                    var bodyAsText = Encoding.UTF8.GetString(buffer);
+
+                    _logger.LogInformation(
+                    @$"Http Response Information : {Environment.NewLine}
+                    Body : {bodyAsText}
+                    Headers : {context.Request.Headers}
+                    Route : {context.Request.Host}");
+
+                    context.Response.Body.Position = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Could not log Response body");
+            }
+
+
+        }
+    }
+}
---
+++ OzonEdu.Infrastructure/Configuration/Middlewares/VersionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace OzonEdu.Infrastructure.Configuration.Middlewares
+{
+    public class VersionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public VersionMiddleware(RequestDelegate next) 
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await GetVersionAndName(context);
+            //TODO: Поэксперементировать с некстом и без
+            //await _next(context);
+        }
+
+        private async Task GetVersionAndName(HttpContext context)
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "no version";
+            var name = Assembly.GetExecutingAssembly().GetName().Name?.ToString() ?? "no name";
+            var versionRespons = new
+            {
+                Version = version,
+                ServiceName = name,
+            };
+            var resultJson = new JsonResult(versionRespons);
+            await context.Response.WriteAsync(JsonSerializer.Serialize(resultJson));
+        }
+    }
+}
