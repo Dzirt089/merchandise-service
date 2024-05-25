999668dc5bb89211ab9a59e3cd754912a0169302
Илья Емец <suoza089@gmail.com>
2024-05-25 21:20:27 +0300

    Перенос подключение Middleware обратно в библиотеку

--- OzonEdu.Infrastructure/Configuration/HostBuilderExtensions.cs
+++ OzonEdu.Infrastructure/Configuration/HostBuilderExtensions.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OzonEdu.MerchandiseService.Infrastructure.Configuration.Filters;
+using OzonEdu.MerchandiseService.Infrastructure.Configuration.Middlewares;
 using OzonEdu.MerchandiseService.Infrastructure.Configuration.StartapFilters;
 using System.Runtime.CompilerServices;
 
@@ -10,16 +12,31 @@ namespace OzonEdu.MerchandiseService.Infrastructure.Configuration
 {
     public static class HostBuilderExtensions
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        //Создаю кофигурацию, чтобы библиотечке считать данные с неё
+        static IConfigurationRoot? _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        /// <summary>
+        /// Подключаем вспомогательные сервисы (Swagger, Logging, <see cref="GlobalExceptionFilter"/> и т.д.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             //Добавляю свои настройки для логов 
-            services.AddLogging(builder =>
+            if(_configuration != null)
             {
-                builder.ClearProviders();
-                builder.AddConfiguration(configuration.GetSection("Logging"));
-                builder.AddConsole();
-                builder.AddFile(@"Logs/app-{Date}.txt");
-            });
+                services.AddLogging(builder =>
+                {
+                    builder.ClearProviders();
+                    builder.AddConfiguration(_configuration.GetSection("Logging"));
+                    builder.AddConsole();
+                    builder.AddFile(@"Logs/app-{Date}.txt");
+                });
+            }
+            
 
             services.AddSwaggerGen();
             services.AddSingleton<IStartupFilter, SwaggerStartupFilter>();
@@ -27,6 +44,17 @@ namespace OzonEdu.MerchandiseService.Infrastructure.Configuration
             services.AddControllers(options => options.Filters.Add<GlobalExceptionFilter>());
 
             return services;
+        }
+
+        /// <summary>
+        /// Подключаем для логгирования Миддлеваре "RequestResponseLoggingMiddleware".   
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder AddInfrastructureMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RequestResponseLoggingMiddleware>();
+            return app;
         }
     }
 }
--- src/OzonEdu.MerchandiseService/Program.cs
+++ src/OzonEdu.MerchandiseService/Program.cs
@@ -7,24 +7,16 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         
-        //Создаю кофигурацию, чтобы библиотечке считать данные с неё
-        IConfigurationRoot _configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
-
-
         builder.Services.AddEndpointsApiExplorer();
         
-        //Подключаем библиотеку почти со всеми подключениями (Сваггер, логги, фильтры, контроллер). Чтобы не засорять основной проект.
-        builder.Services.AddInfrastructure(_configuration);
+        //Подключаем сервисы библиотеки почти со всеми подключениями (Сваггер, логги, фильтры, контроллер).        
+        builder.Services.AddInfrastructure();
         builder.Services.AddSingleton<IMerchService, MerchService>();
 
         var app = builder.Build();
 
-        //Подключаем Миддлеваре из библиотеки "RequestResponseLoggingMiddleware", напрямую тут.
-        //Пока не разобрался, как в библиотеке "Infrastructure" подключить, чтобы работало.
-        app.UseMiddleware<RequestResponseLoggingMiddleware>();
+        //Подключаем миддлеваре библиотеки
+        app.AddInfrastructureMiddleware();
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
