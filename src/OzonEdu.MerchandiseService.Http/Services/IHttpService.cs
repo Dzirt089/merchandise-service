7c35a3b914c3dcfc4c2384f8e8c0935d6b8d0831
Илья Емец <suoza089@gmail.com>
2026-03-24 14:22:40 +0300

    удалил легаси

--- src/OzonEdu.MerchandiseService.Http/Services/IHttpService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OzonEdu.MerchandiseService.Http.Services
-{
-	public interface IHttpService
-	{
-		Task<List<ItemPosition>> GetAll(CancellationToken token);
-		Task<ItemPosition> GetById(long id, CancellationToken token);
-	}
-}
